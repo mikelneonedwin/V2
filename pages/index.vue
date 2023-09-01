39bf2ac0104b743887190d6076faa625092ed584
Mikel Edwin <michealanthonyxxx@gmail.com>
2023-09-01 19:18:06 +0100

    reinstated codes for uploads

--- pages/index.vue
+++ pages/index.vue
@@ -48,15 +48,14 @@
         </div>
         <div class="flex flex-col gap-2">
           <div class="flex w-full">
-            <i
-              class="not-italic inline-block text-center"
-              style="width: calc(100% / 30)"
-              :class="i == form.day ? 'text-blue-500 font-semibold' : ''"
-              v-for="i in count"
-              >{{ i }}</i
-            >
+            <i v-if="width" class="not-italic inline-block text-center" style="width: calc(100% / 30)" :class="i == form.day ? 'text-blue-500 font-semibold' : ''" v-for="i in count">{{ i }}</i>
+            <i v-else class="not-italic font-bold text-blue-500">{{ form.day }}</i>
           </div>
           <input type="range" v-model="form.day" min="1" max="30" />
+          <div>
+            <button type="button" @click="inc('-')" class="opt material-icons">arrow_left</button>
+            <button type="button" @click="inc('+')" class="opt float-right material-icons">arrow_right</button>
+          </div>
         </div>
       </div>
       <Opt txt="Frameworks & Libraries" :opts="frameworks" />
@@ -77,7 +76,7 @@
       <i v-if="error.file" class="error">Upload at least one file</i>
       <div class="grid grid-cols-1 gap-2">
         <div
-          v-for="file in files"
+          v-for="file in files.filter(a => a.name != 'desc.txt')"
           :key="file.name"
           class="flex items-start gap-2"
         >
@@ -88,11 +87,12 @@
             <div class="flex gap-2">
               <span>Type: {{ file.type }}</span>
               <span>Size: {{ (file.size / 1024 ** 2).toFixed(2) }} MB</span>
+              <span class="text-red-400" @click="files.splice(files.indexOf(file), 1)">Remove</span>
             </div>
           </div>
         </div>
       </div>
-      <button class="opt py-2" type="submit">SUBMIT</button>
+      <button class="opt py-2" type="submit">{{status}}</button>
     </form>
     <input ref="input" @change="addFile" type="file" class="hidden" max="1" />
     <div
@@ -111,6 +111,8 @@
 </template>
 
 <script setup>
+const status = ref('SUBMIT');
+const width = process.client ? innerWidth > 1023 : true;
 const count = Array(30)
   .fill(1)
   .map((a, b) => a + b);
@@ -125,6 +127,11 @@ const form = reactive({
   day,
   repo: "",
 });
+function inc(o){
+  form.day = Number(form.day);
+  form.day += o == '+' ? 1 : -1;
+  form.day = !form.day ? 1 : form.day > 30 ? 30 : form.day;
+}
 const transform = (array) =>
   reactive(array.sort().map((a) => ({ txt: a, opt: false })));
 const frameworks = transform([
@@ -192,9 +199,7 @@ function submit() {
   const ans = {
     day: form.day,
     completed: form.production,
-    desc: `Day ${form.day}\n\n Status: ${
-      form.production ? "Production" : "Development"
-    }\n`,
+    desc: `Day ${form.day}\n\nStatus: ${ form.production ? "Production" : "Development" }\n`,
     files,
   };
   if (form.link) {
@@ -215,7 +220,25 @@ function submit() {
   if (usedTools.length)
     ans.desc += `Tools (Hosting & Database): ${usedTools.join(", ")}\n`;
   ans.desc += `\nDescription: ${form.desc}`;
-  console.log(ans);
+  const formData = new FormData();
+  const desBlob = new Blob([ans.desc], {type:"text/plain"});
+  desBlob.name = "desc.txt";
+  files.push(desBlob);
+  for(const key in ans){
+    if(key == "files") continue;
+    formData.append(key, ans[key]);
+  }
+  for(const file of ans.files) formData.append(file.name, file, file.name);
+  status.value = "Loading...";
+  $fetch('/api/asp', {
+    method: 'POST',
+    body: formData
+  }).then(() => {
+    status.value = "SUBMITTED!";
+  }).catch(err => {
+    console.error(err);
+    status.value = "SUBMIT";
+  })
 }
 </script>
 
