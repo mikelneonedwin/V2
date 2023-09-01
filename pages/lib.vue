b26f9cd7cf47cd2621e694f5b585f7e0943af164
Mikel Edwin <michealanthonyxxx@gmail.com>
2023-09-01 11:50:59 +0100

    Placeholder for /lib

---
+++ pages/lib.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="lg:w-3/4 lg:mx-auto">
+        <h1 class="text-center text-4xl font-bold">THIS PAGE IS UNDER DEVELOPMENT</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
